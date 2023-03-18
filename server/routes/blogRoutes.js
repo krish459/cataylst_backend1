9a8574857a8687ab79c31d224c4ed049aa83ba7b
Krish <krishshah235@gmail.com>
2023-03-18 17:43:25 +0530

    Blog by Id route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -30,7 +30,6 @@ const router = express.Router();
  *    description: The Blog managing API
  */
 
-
 /**
  * @swagger
  * /api/blog/get-blogs:
@@ -48,28 +47,20 @@ const router = express.Router();
  *                    items:
  *                        $ref: '#/components/schemas/Blog'
  */
-router.get(
-    "/get-blogs",
-    async (req, res) => {
-      try {
-  
-        const { page, perPage } = req.query;
-        const options = {
-          page: parseInt(page, 10) || 1,
-          limit: parseInt(perPage, 10) || 3,
-        };
-        
-        const blogs = await Blog.paginate({},options);
-        res.status(200).json({ blogs });
-        
-  
-      }catch(error) {
-        return res.status(400).json({ message: `${error}` });
-      } 
-    }
-  );
+router.get("/get-blogs", async (req, res) => {
+  try {
+    const { page, perPage } = req.query;
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: parseInt(perPage, 10) || 3,
+    };
 
-
+    const blogs = await Blog.paginate({}, options);
+    res.status(200).json({ blogs });
+  } catch (error) {
+    return res.status(400).json({ message: `${error}` });
+  }
+});
 
 /**
  * @swagger
@@ -95,30 +86,68 @@ router.get(
  *         description: The request data was invalid or incomplete.
  */
 
-  router.post(
-    "/create-blog",
-    async (req, res) => {
-      try {
-        const { img, postMeta, title, postDescriptions } = req.body;
-  
-        if (!postMeta || !title || !postDescriptions) {
-          return res.status(400).json({ message: "Post meta, title, and descriptions are required." });
-        }
-  
-        const newBlog = new Blog({
-          img,
-          postMeta,
-          title,
-          postDescriptions,
-        });
-  
-        const savedBlog = await newBlog.save();
-  
-        res.status(201).json({ blog: savedBlog });
-  
-      } catch (error) {
-        return res.status(400).json({ message: `${error}` });
-      }
+router.post("/create-blog", async (req, res) => {
+  try {
+    const { img, postMeta, title, postDescriptions } = req.body;
+
+    if (!postMeta || !title || !postDescriptions) {
+      return res
+        .status(400)
+        .json({ message: "Post meta, title, and descriptions are required." });
     }
-  );
+
+    const newBlog = new Blog({
+      img,
+      postMeta,
+      title,
+      postDescriptions,
+    });
+
+    const savedBlog = await newBlog.save();
+
+    res.status(201).json({ blog: savedBlog });
+  } catch (error) {
+    return res.status(400).json({ message: `${error}` });
+  }
+});
+
+/**
+ * @swagger
+ * /api/blog/get-blog/{id}:
+ *   get:
+ *     summary: Get a single blog by ID.
+ *     tags: [blogs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the blog to get.
+ *     responses:
+ *       '200':
+ *         description: A single blog object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Blog'
+ *       '404':
+ *         description: Blog with provided ID not found.
+ */
+
+router.get("/get-blog/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json({ blog });
+  } catch (error) {
+    return res.status(400).json({ message: `${error}` });
+  }
+});
+
 module.exports = router;
